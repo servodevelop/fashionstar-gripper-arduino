7eab8f886ec0ea37288cfa0700efbce4825ec47a
Shunkai Xing <xingshunkai@qq.com>
2021-07-15 14:39:01 +0800

    调整时间

--- examples/gripper_set_angle/gripper_set_angle.ino
+++ examples/gripper_set_angle/gripper_set_angle.ino
@@ -3,18 +3,28 @@
  * --------------------------
  * 作者: 阿凯|Kyle
  * 邮箱: kyle.xing@fashionstar.com.hk
- * 更新时间: 2020/11/14
+ * 更新时间: 2021/07/15
  */
  
-#include <SoftwareSerial.h>
 #include "FashionStar_UartServoProtocol.h"  // 串口总线舵机通信协议
 #include "FashionStar_UartServo.h"          // Fashion Star串口总线舵机
 #include "FashionStar_SmartGripper.h"       // Fashion Star智能夹具
 
-// 软串口的配置
-#define SOFT_SERIAL_RX 6    // 软串口发送端的GPIO
-#define SOFT_SERIAL_TX 7    // 软串口接收端的GPIO
-#define SOFT_SERIAL_BAUDRATE 4800   // 软串口的波特率
+// 调试串口的配置
+#if defined(ARDUINO_AVR_UNO)
+    #include <SoftwareSerial.h>
+    #define SOFT_SERIAL_RX 6
+    #define SOFT_SERIAL_TX 7
+    SoftwareSerial softSerial(SOFT_SERIAL_RX, SOFT_SERIAL_TX); // 创建软串口
+    #define DEBUG_SERIAL softSerial
+    #define DEBUG_SERIAL_BAUDRATE 4800
+#elif defined(ARDUINO_AVR_MEGA2560)
+    #define DEBUG_SERIAL Serial
+    #define DEBUG_SERIAL_BAUDRATE 115200
+#elif defined(ARDUINO_ARCH_ESP32)
+    #define DEBUG_SERIAL Serial
+    #define DEBUG_SERIAL_BAUDRATE 115200
+#endif 
 
 // 串口总线舵机配置
 #define SERVO_ID 4      //爪子对应的舵机ID号
@@ -26,8 +36,6 @@
 #define GRIPPER_INTERVAL_MS 1000        // 爪子开启闭合的周期, ms
 #define GRIPPER_MAX_POWER 400           // 爪子的最大功率
 
-// 软串口初始化
-SoftwareSerial softSerial(SOFT_SERIAL_RX, SOFT_SERIAL_TX); // 创建软串口
 // 创建舵机的通信协议对象
 FSUS_Protocol protocol(BAUDRATE);
 // 创建舵机的实例
@@ -36,17 +44,17 @@ FSUS_Servo uservo(SERVO_ID, &protocol);
 FSGP_Gripper gripper(&uservo, SERVO_ANGLE_GRIPPER_OPEN,SERVO_ANGLE_GRIPPER_CLOSE);
 
 void setup(){
-    softSerial.begin(SOFT_SERIAL_BAUDRATE); // 软串口
-    softSerial.println("Start To Test Gripper\n"); // 打印日志
+    DEBUG_SERIAL.begin(DEBUG_SERIAL_BAUDRATE); // 软串口
+    DEBUG_SERIAL.println("Start To Test Gripper\n"); // 打印日志
     
     protocol.init();    // 舵机通信协议初始化    
     gripper.init();     // 爪子初始化
 }
 
 void loop(){
-    softSerial.println("Gripper Close\n"); // 打印日志
+    DEBUG_SERIAL.println("Gripper Close\n"); // 打印日志
     gripper.setAngle(SERVO_ANGLE_GRIPPER_CLOSE, GRIPPER_INTERVAL_MS, GRIPPER_MAX_POWER);
-    delay(10000); // 等待1s
-    softSerial.println("Gripper Open\n"); // 打印日志
+    delay(5000); // 等待5s
+    DEBUG_SERIAL.println("Gripper Open\n"); // 打印日志
     gripper.setAngle(SERVO_ANGLE_GRIPPER_OPEN, GRIPPER_INTERVAL_MS, GRIPPER_MAX_POWER);
 }
